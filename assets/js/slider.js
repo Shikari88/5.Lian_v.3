f8f0d7b680797f859c2b643193cb29023a45a0b4
User <astakhin.rsm@mail.ru>
2020-07-07 17:44:53 +0300

    доделана разметка index.html и большинство стилей. Доделать остатки стилей

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -5,9 +5,31 @@ $(function(){
     let slidesCount = container.find('li').length; // ищем все теги li в ul и считаем их колличество.
     let containerWidth = slidesCount * slider.width();
     let originWidth = slider.width();
+    let next = $('.slider__next');
+    let prev = $('.slider__prev');
+    let n = 1;
+    let changeSlide = function () {
+        // меняем свойство left для ul c анимацией
+        container.animate({left: -n * slider.width() + 'px'}, 800);
+        // вызываем функцию, которая скрывает или показывает кнопки вперёд-назад
+    };
+    next.click(function () {
+        if (n < slidesCount - 1) {
+            // увеличиваем номер текущего слайда
+            n++;
+            changeSlide();
+        }
+    });
 
+    prev.click(function () {
+        if (n > 0) {
+            // уменьшаем номер текущего слайда
+            n--;
+            changeSlide();
+        }
+    });
     //создаем код для нафигационных точек. Создаем разметку .slider__nav>span*2
-    let navHtml = '<div class="slider__nav">';
+    /*let navHtml = '<div class="slider__nav">';
     let i = 0;
     while (i < slidesCount) {
         navHtml += '<span data-n="'+ i+ '"></span>';
@@ -17,15 +39,9 @@ $(function(){
     slider.append(navHtml);//Вставляет содержимое, заданное параметром, в конец каждого элемента в наборе соответствующих элементов
 
     //номер текущего слайда
-    let n = 0;
 
 
-    let changeSlide = function () {
-        // меняем свойство left для ul c анимацией
-        container.animate({left: -n * slider.width() + 'px'}, 800);
-        // вызываем функцию, которая скрывает или показывает кнопки вперёд-назад
 
-    };
 
     //находим все кнопки навигации снизу слайда
     let dots = $('.slider__nav span');
@@ -33,5 +49,5 @@ $(function(){
     dots.click(function () {
         n = $(this).data('n');
         changeSlide();
-    })
+    })*/
 });
