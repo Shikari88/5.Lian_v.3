75cfeaf0416c5cd6457a755f0829618f09ee9bae
User <astakhin.rsm@mail.ru>
2020-09-09 15:31:39 +0300

    прописана адаптивность под 1024 и 768

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,24 +3,26 @@
 $(function () {
     const headerSearchBtn = $('.header__search');
     const searchForm = $('.header__search-form');
+    //Показываем/скрываем поле ввода поиска
     headerSearchBtn.click(function () {
-        searchForm.show(function () {
+        searchForm.toggle(function () {
             searchForm.animate();
         });
     });
 
-
-
-
+    //Переключем страницы по навтгации проектов
     $('.product-cards__nav-item').click(function () {
-        let href = $(this).attr('href');// сохраняем значение атрибута href, по которому был произведен клик
-
+        let href = $(this).attr('href');
         $('.product-cards__nav-item, .product-cards__inner').removeClass('active');
         $(href).addClass('active');
         $(this).addClass('active');
         $('html, body').animate({scrollTop:645},1000);
+        $('.product-cards__list--hide').hide();
     });
 
+    $('.btn--more').click(function (){
+        $('.list--hide').slideToggle();
+    });
 
 
 });
