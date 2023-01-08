c9812f0ee7f411616d68c6bfc01cc5a4c79d2e1f
Shikari88 <astakhin.rsm@mail.ru>
2023-01-08 10:53:54 +0300

    возобновление работы над проектом. Тестово закинут на хост в данном виде.

--- assets/js/menuBurger.js
+++ assets/js/menuBurger.js
@@ -1,15 +1,15 @@
-// $(function(){
-//
-//     $('.menuBurger').on('click', function(){
-//
-//         $('.nav__list').slideToggle(300, function(){
-//
-//             if($(this).css('display') === 'none'){
-//                 $(this).removeAttr('style');
-//             }
-//
-//         });
-//
-//     });
-//
-// });
+$(function(){
+
+    $('.menuBurger').on('click', function(){
+
+        $('.nav__list').slideToggle(300, function(){
+
+            if($(this).css('display') === 'none'){
+                $(this).removeAttr('style');
+            }
+
+        });
+
+    });
+
+});
