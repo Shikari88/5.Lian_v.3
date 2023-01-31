a04f82042c10fb049f0f5e3153cf493f9575d706
Shikari88 <astakhin.rsm@mail.ru>
2023-01-31 19:19:02 +0300

    wreeblo:  - построен header и главный экран booksy:  - добавлена адаптация под моб устройства. MVP готов

--- assets/portfolio/booksy/assets/js/form.js
+++ assets/portfolio/booksy/assets/js/form.js
@@ -7,47 +7,25 @@ window.onload = function () {
     let popup = document.querySelector('#popup');
     let reg = document.getElementById("registration");
 
-
-
-
     for (let i = 0; i < helpIcon.length; i++) {
         for (let j = 0; j < formHelp.length; j++) {
             helpIcon[i].addEventListener("click", function () {
                 if (i === j) {
                     formHelp[j].classList.toggle("show");
                     popup.style.display = "block";
-
-
                     let yu = reg.offsetHeight;
                     popup.style.height = yu + "px";
-
-
-
-                    // setTimeout(function () {
-                    //     formHelp[j].style.display = "none";
-                    // }, 3000);
                 }
-
-
             });
-
-
-            // document.body.addEventListener("click", () => {
-            //     formHelp[j].classList.toggle("hide");
-            // })
         }
     }
 
-    //let qwe = reg.getAttribute(height);
-    //console.log(reg);
     if (popup.style.display === "block") {
         let yu = reg.offsetHeight;
         popup.setAttribute(height, yu);
         console.log(popup.offsetHeight);
 
     }
-
-
 
     for (let k = 0; k < formHelp.length; k++) {
         popup.addEventListener('click', () => {
@@ -56,29 +34,6 @@ window.onload = function () {
         });
     }
 
-
-    // let headerLogo = document.querySelector(".header__logo");
-    //
-    // for (let k = 0; k < formHelp.length; k++) {
-    //     headerLogo.addEventListener("click", (e) => {
-    //
-    //         formHelp[k].classList.remove("show");
-    //
-    //
-    //     });
-    //
-    // }
-
-
-    // for (let k = 0; k < formHelp.length; k++) {
-    //     document.addEventListener("click", (e) => {
-    //         const withinBoundaries = e.composedPath().includes(formHelp[k]);
-    //         if ( ! withinBoundaries ) {
-    //             formHelp[k].style.display = 'none'; // скрываем элемент т к клик был за его пределами
-    //         }
-    //     })
-    // }
-    //-----------------------------------------------------------------
 
     //----показывам выпадающее меню при клике на иконку стрелки--------
     let formText = document.querySelectorAll('.form__text');
@@ -138,8 +93,6 @@ window.onload = function () {
     //         isFreePrise.disabled = false;
     //     }
     // });
-
-
     // isFree.addEventListener('click', () => {
     //     if (isFree.checked) {
     //         // console.log(prices[0]);
@@ -157,53 +110,6 @@ window.onload = function () {
     //         }
     //     }
     // });
-
-
-
-
-    //-----Окно успешной регистрации при клике на кнопку-----
-    // let btn = document.getElementById("btn");
-    // btn.addEventListener("click", ()=> {
-    //     window.location.href = "succeful-reg.html";
-    // })
-
-
-    // let btn = document.getElementById("btn");
-    // let popupSuccess = document.getElementById('popupSuccess');
-    //
-    // function showPopup(btn) {
-    //     btn.preventDefault();
-    //     popupSuccess.style.display = "block";
-    //     setTimeout(function () {
-    //         popupSuccess.style.display = "none";
-    //     },3000);
-    //
-    // }
-    // btn.addEventListener("click", showPopup);
-
-
-    // document.body.onkeydown = function (ev) {
-    //     if (ev.keyCode === 27) {
-    //         popupSuccess.style.display = "none";
-    //     }
-    // };
-
-    //
-    //     popupSuccess.style.display = "none";
-    // },5000);
-
-    //----Попытука сделать всплывающее окно успешной регитсрации при клике на кнопку зарегаться
-
-    // let button = document.getElementById('btn');
-    // let popupSuccess = document.getElementById('popupSuccess');
-    // button.addEventListener("click", function () {
-    //     popupSuccess.style.display = "block";
-    // })
-
-
-    // if(isFreePrise.checked) {
-    //     labelIsFreePrise.innerHTML = "sasa";
-    // }
 
 
     //----Попытука выделить все чекбоксы при клике на главный
@@ -226,67 +132,4 @@ window.onload = function () {
     //
     //     })
     // }
-
-
-
-
-    // let i = 0;
-    // while(i < helpIcon.length) {
-    //     helpIcon[i].onclick = function () {
-    //         formHelp.style.display = "block";
-    //     };
-    //     i++;
-    // }
-
-    // function showHelp () {
-    //     //Виснет с таким говнокодом
-    //     // let i = 0;
-    //     // while (i < formHelp.length) {
-    //     //     formHelp[i].style.display = "block";
-    //     // }
-    //     // i++;
-    //     formHelp.style.display = "block";
-    // }
-    //
-    // helpIcon.addEventListener("click", showHelp);
-
-
-    // -----------------рабочий вариант-----------------------
-
-    // for(let i = 0; i < helpIcon.length; i++) {
-    //     helpIcon[i].onclick = function () {
-    //         for(let j = 0; j < formHelp.length; j++) {
-    //             if(i === j) {
-    //                 formHelp[j].style.display = "block";
-    //                 setTimeout(function () {
-    //                     formHelp[j].style.display = "none";
-    //                 }, 3000);
-    //             }
-    //         }
-    //     }
-    // }
-
-    //----------------------------------------------------------
-
-    // for(let i = 0; i < helpIcon.length; i++) {
-    //     helpIcon[i].addEventListener("click", function(){
-    //         for(let j = 0; j < formHelp.length; j++) {
-    //             if(i === j) {
-    //                 formHelp[j].style.display = "block";
-    //                 setTimeout(function () {
-    //                     formHelp[j].style.display = "none";
-    //                 }, 3000);
-    //             }
-    //         }
-    //     });
-    // }
-
-
-
-    // let box = document.getElementsByClassName('box');
-    // for(let i = 0; i < box.length; i++) {
-    //     console.log(box.length);
-    // }
-
-
 };
