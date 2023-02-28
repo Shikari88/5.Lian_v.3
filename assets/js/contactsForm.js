3e3f6bafb94e2673259447e1e2a74f55a8da6766
Shikari88 <astakhin.rsm@mail.ru>
2023-02-28 17:15:38 +0400

    переделана стилизация карточек проектов (.product-card)

---
+++ assets/js/contactsForm.js
@@ -0,0 +1,16 @@
+
+
+//добавляем сообщение при успешной отправки форымы контактов
+// window.onload = function(){
+//     const contactsForm = new contactsForm({
+//         el: "#contactsForm",
+//         data: {
+//           titleConfirm: "Соощение успешно отправлено."
+//         },
+//         method: {
+//             showConf: function () {
+//                 this.alert("Соощение успешно отправлено.");
+//             }
+//         }
+//     })
+// };
